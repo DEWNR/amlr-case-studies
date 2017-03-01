1fb7964a1bab0eed3a8250d23af8bab2754a715c
Nick <NWelsh@env.sa.gov.au>
2017-03-01 14:14:06 +1030

    many things

--- src/javascripts/controller.js
+++ src/javascripts/controller.js
@@ -1,5 +1,32 @@
-angular.module('case-studies', [])
-.controller('StudiesController', function StudiesController() {
+var app = angular.module('caseStudies', []);
 
+app.controller('StudiesController', function($scope, $http) {
+	$http.get('js/studies-data.json')
+		.then(function(res){
+			$scope.allData = res.data;
+			// $scope.themes = res.data.themes;
+			// $scope.yrLevel = res.data.yrLevel;
+			// $scope.progress = res.data.progress;
+			// $scope.EfSPrinciples = res.data.EfSPrinciples;
+			// $scope.clearFilter = false;
+		});
+
+	$scope.clearFilter = function() {
+		$scope.search.themes = '';
+  //   	$scope.allData = res.data;
+  //   	$scope.themes = res.data.themes;
+		// $scope.yrLevel = res.data.yrLevel;
+		// $scope.progress = res.data.progress;
+		// $scope.EfSPrinciples = res.data.EfSPrinciples;
+	};
+
+	// function clearFilter() {
+	// 	console.log('here??');
+ //    	$scope.allData = null;
+ //    	$scope.themes = res.data.themes;
+	// 	$scope.yrLevel = res.data.yrLevel;
+	// 	$scope.progress = res.data.progress;
+	// 	$scope.EfSPrinciples = res.data.EfSPrinciples;
+	// }
 
 });
