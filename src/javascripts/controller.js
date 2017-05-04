8558735b44e4844dbc281d96f314f3a450bbeee5
Nick <NWelsh@env.sa.gov.au>
2017-05-04 15:17:02 +0930

    controller notes

--- src/javascripts/controller.js
+++ src/javascripts/controller.js
@@ -4,7 +4,19 @@ app.controller('StudiesController', function($scope, $http) {
 	$http.get('js/studies-data.json')
 		.then(function(res){
 			$scope.allData = res.data;
+
+            // TODO: restrict Search box to title's and schools
+            // $scope.search = '';
+            // $scope.search.titleandschool = $scope.allData.map(function(study) {
+            //     return {
+            //         title: study.title,
+            //         schools: study.schools
+            //     }
+            // });
+
 		});
+
+
 
 	$scope.clearFilter = function() {
 		// $scope.search.$ = '';
