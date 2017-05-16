a6eab41c57e67abde0202f38c8b8fcd25b2d5a69
Nick <NWelsh@env.sa.gov.au>
2017-05-16 17:48:19 +0930

    update to handle varying URL's and the Seamless Templates URL injections

--- src/javascripts/controller.js
+++ src/javascripts/controller.js
@@ -1,3 +1,5 @@
+
+
 var app = angular.module('caseStudies', []);
 
 app.controller('StudiesController', ['$scope', '$http', function($scope, $http) {
@@ -24,7 +26,15 @@ app.controller('StudiesController', ['$scope', '$http', function($scope, $http) 
 
 }]);
 
+// angular.config(function($locationProvider) {
+//     $locationProvider.html5Mode({
+//         enabled: true,
+//         requireBase: false,
+//         rewriteLinks: false
+//     });
+// });
 
+// Comment out below before uploading to website. A DLV will create this.
 var allData = [
  {
    "title": "Ecological Conversion: An Education for Sustainability Learning Story",
@@ -35,8 +45,8 @@ var allData = [
    "themes": "Biodiversity, Food garden",
    "date": 2014,
    "author": "Jeremy",
-   "studyURL": "studies/study1.pdf",
-   "imageURL": "images/pic268x150.png"
+   "studyURL": "http://localhost:3000/studies/study1.pdf",
+   "imageURL": "http://localhost:3000/images/pic268x150.png"
  },
  {
    "title": "An Education for Sustainability learning story from Allenby Gardens Primary School",
@@ -47,6 +57,89 @@ var allData = [
    "themes": "Food garden",
    "date": 2015,
    "author": "Amy?",
-   "studyURL": "studies/case-study-2.pdf",
-   "imageURL": "//localhost:3002/images/horsnell-scrub-view.jpg"
+   "studyURL": "http://localhost:3000/studies/case-study-2.pdf",
+   "imageURL": "//localhost:3000/images/horsnell-scrub-view.jpg"
  }];
+
+
+
+// Rewrite paths fix for SeamlessCMS's poor URL handling in templates
+allData.forEach(function(part, index, theArray) {
+
+    var study = theArray[index];
+
+    // regex replaces "http://" and "//"
+    study.imageURL = study.imageURL.replace(/https:\/\/|http:\/\/|\/\/|/gi, '');
+
+    // regex replaces "http://" and "//"
+    study.studyURL = study.studyURL.replace(/https:\/\/|http:\/\/|\/\/|/gi, '');
+    study.studyURL = study.studyURL.replace(/\/files\/sharedassets/gi, 'www.environment.sa.gov.au/files/sharedassets');
+
+});
+
+
+
+/* Polyfill for .forEach function. */
+// Production steps of ECMA-262, Edition 5, 15.4.4.18
+// Reference: http://es5.github.io/#x15.4.4.18
+if (!Array.prototype.forEach) {
+
+  Array.prototype.forEach = function(callback/*, thisArg*/) {
+
+    var T, k;
+
+    if (this == null) {
+      throw new TypeError('this is null or not defined');
+    }
+
+    // 1. Let O be the result of calling toObject() passing the
+    // |this| value as the argument.
+    var O = Object(this);
+
+    // 2. Let lenValue be the result of calling the Get() internal
+    // method of O with the argument "length".
+    // 3. Let len be toUint32(lenValue).
+    var len = O.length >>> 0;
+
+    // 4. If isCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    // 5. If thisArg was supplied, let T be thisArg; else let
+    // T be undefined.
+    if (arguments.length > 1) {
+      T = arguments[1];
+    }
+
+    // 6. Let k be 0
+    k = 0;
+
+    // 7. Repeat, while k < len
+    while (k < len) {
+
+      var kValue;
+
+      // a. Let Pk be ToString(k).
+      //    This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty
+      //    internal method of O with argument Pk.
+      //    This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+
+        // i. Let kValue be the result of calling the Get internal
+        // method of O with argument Pk.
+        kValue = O[k];
+
+        // ii. Call the Call internal method of callback with T as
+        // the this value and argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
+}
